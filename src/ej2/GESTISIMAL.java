b695db67ca58b42cf8a3222fbff209294958929a
enieves <enieves@107-07.info2>
2024-03-07 11:03:04 +0100

    terminar ej2

---
+++ src/ej2/GESTISIMAL.java
@@ -0,0 +1,39 @@
+package ej2;
+
+import java.util.Scanner;
+
+public class GESTISIMAL {
+
+
+	/**
+	 * titulo del producto
+	 */
+	private String titulo;
+
+	/**
+	 * descripcion del producto
+	 */
+	private String descripcion;
+
+	/**
+	 * precio de compra
+	 */
+	private double precioCompra=0;
+
+	/**
+	 * precio de venta
+	 */
+	private double precioVenta=0;
+	
+	/**
+	 * nº de unidades
+	 */
+	private int stock=0;
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/ej2/Main.java
@@ -0,0 +1,13 @@
+package ej2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		
+	}
+
+}
