c787a343cf93afc429ce15d17cfa9f8dc47ba642
enieves <enieves@106-02.info2>
2024-03-21 12:18:41 +0100

    for each en pizzas

--- src/ej3/ArrayPizza.java
+++ src/ej3/ArrayPizza.java
@@ -1,14 +1,69 @@
 package ej3;
 
+
 public class ArrayPizza {
 
-	Pizza[] pizza=new Pizza[50];
+	public static Pizza[] pizzas=new Pizza[50];
+	
+	
+
+	public static void listarPizzas() {
+		for(Pizza p : pizzas) {
+			if(p != null) {
+				System.out.println(p);
+			}
+		}
+	}
 	
 	
 	
+	/**
+	 * comprueba si hay una posicion libre o no, es decir, que sea null
+	 * @return la comprobacion
+	 */
+	public static int posLibre() {
+		int posicion=-1;
+		
+		int i=0;
+		
+		//recorremos el array hasta que el valor sea nulo
+		while(i<pizzas.length&&posicion!=-1) {
+			if(pizzas[i]==null) {
+				//
+				posicion=i;
+			}
+			
+		}
+		
+		
+		
+		return posicion;
+	}
 	
 	
-	
+	public static boolean anadirPizza(Pizza pizza) {
+		
+		boolean comprobacion=false;
+		
+		boolean busca;
+		
+		int i=0;
+		
+		//recorremos el array hasta que el valor sea nulo
+		while(i<pizzas.length&&!comprobacion) {
+			if(pizzas[i]==null) {
+				
+				//
+				comprobacion=true;
+			}
+			
+		}
+		
+		
+		
+		return comprobacion;
+		
+	}
 	
 	
 	
